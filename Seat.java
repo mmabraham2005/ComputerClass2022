110eb4b916412bdcaed461ea56ae76e44d822e51
mmabraham2005 <mmabraham2005@gmail.com>
2022-10-22 15:11:00 -0700

    reformatted Seat.Java

--- Seat.java
+++ Seat.java
@@ -2,30 +2,29 @@ public class Seat
 
 {
 
-private boolean available;
+    private boolean available;
 
-private int tier;
+    private int tier;
 
-public Seat(boolean isAvail, int tierNum)
+    public Seat(boolean isAvailable, int tierNum)
+    {
+        available = isAvailable;
+        tier = tierNum;
+    }
 
-{
+    public boolean isAvailable()
+    {
+        return available;
+    }
 
-available = isAvail;
+    public int getTier()
+    {  
+        return tier;
+    }
 
-tier = tierNum;
-
-}
-
-public boolean isAvailable()
-
-{  return available;  }
-
-public int getTier()
-
-{  return tier;  }
-
-public void setAvailability(boolean isAvail)
-
-{  available = isAvail;  }
+    public void setAvailability(boolean isAvail)
+    {  
+        available = isAvail;  
+    }
 
 }
